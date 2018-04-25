5d14f781da4285fce7209f4c29e2603b9286a927
Ryo Takashima <r.takashima@miniascape.co.jp>
2018-04-25 10:19:54 +0900

    1-4samples

---
+++ appengine/gophers/gophers-3/main.go
@@ -0,0 +1,60 @@
+// Copyright 2018 Google Inc. All rights reserved.
+// Use of this source code is governed by the Apache 2.0
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+
+	"google.golang.org/appengine"
+)
+
+var (
+	indexTemplate = template.Must(template.ParseFiles("index.html"))
+)
+
+type templateParams struct {
+	Notice string
+	Name   string
+}
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	appengine.Main()
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.Redirect(w, r, "/", http.StatusFound)
+		return
+	}
+	params := templateParams{}
+
+	if r.Method == "GET" {
+		indexTemplate.Execute(w, params)
+		return
+	}
+
+	// It's a POST request, so handle the form submission.
+	name := r.FormValue("name")
+	params.Name = name // Preserve the name field.
+	if name == "" {
+		name = "Anonymous Gopher"
+	}
+
+	if r.FormValue("message") == "" {
+		w.WriteHeader(http.StatusBadRequest)
+
+		params.Notice = "No message provided"
+		indexTemplate.Execute(w, params)
+		return
+	}
+
+	// TODO: save the message into a database.
+
+	params.Notice = fmt.Sprintf("Thank you for your submission, %s!", name)
+	indexTemplate.Execute(w, params)
+}
---
+++ appengine/gophers/gophers-4/main.go
@@ -0,0 +1,99 @@
+// Copyright 2018 Google Inc. All rights reserved.
+// Use of this source code is governed by the Apache 2.0
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"google.golang.org/appengine"
+	"time"
+	"google.golang.org/appengine/datastore"
+	"google.golang.org/appengine/log"
+)
+
+var (
+	indexTemplate = template.Must(template.ParseFiles("index.html"))
+)
+
+type Post struct {
+	Author  string
+	Message string
+	Posted  time.Time
+}
+
+type templateParams struct {
+	Notice string
+	Name string
+	Message string
+	Posts []Post
+}
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	appengine.Main()
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.Redirect(w, r, "/", http.StatusFound)
+		return
+	}
+	// [START new_context]
+	ctx := appengine.NewContext(r)
+
+	params := templateParams{}
+
+	q := datastore.NewQuery("Post").Order("-Posted").Limit(20)
+	if _, err := q.GetAll(ctx, &params.Posts); err != nil {
+		log.Errorf(ctx, "Getting posts: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		params.Notice = "Couldn't get latest posts. Refresh?"
+		indexTemplate.Execute(w, params)
+		return
+	}
+
+	if r.Method == "GET" {
+		indexTemplate.Execute(w, params)
+		return
+	}
+
+	// It's a POST request, so handle the form submission.
+	post := Post{
+		Author:  r.FormValue("name"),
+		Message: r.FormValue("message"),
+		Posted:  time.Now(),
+	}
+
+	if post.Author == "" {
+		post.Author = "Anonymous Gopher"
+	}
+	params.Name = post.Author
+
+	if post.Message == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		params.Notice = "No message provided"
+		indexTemplate.Execute(w, params)
+		return
+	}
+
+	// [START new_key]
+	key := datastore.NewIncompleteKey(ctx, "Post", nil)
+	if _, err := datastore.Put(ctx, key, &post); err != nil {
+		log.Errorf(ctx, "datastore.Put: %v", err)
+
+		w.WriteHeader(http.StatusInternalServerError)
+		params.Notice = "Couldn't add new post. Try again?"
+		params.Message = post.Message // Preserve their message so they can try again.
+		indexTemplate.Execute(w, params)
+		return
+	}
+
+	// Prepend the post that was just added.
+	params.Posts = append([]Post{post}, params.Posts...)
+
+	params.Notice = fmt.Sprintf("Thank you for your submission, %s!", post.Author)
+	indexTemplate.Execute(w, params)
+}
